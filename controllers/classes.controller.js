5ca02f5f58890cc71bd0523d2d1b28cf9ed12163
shahjalal-bu <mdshahjalal.bu@gmail.com>
2023-06-12 05:35:08 +0600

    approved and denied class done

--- controllers/classes.controller.js
+++ controllers/classes.controller.js
@@ -1,33 +1,15 @@
 const { classesCollection, usersCollection } = require("../db");
 const { ObjectId } = require("mongodb");
 
+//find all classes
+
 module.exports.findAll = async (req, res) => {
   let classes = await classesCollection.find().toArray();
   res.send(classes);
 };
 
-// module.exports.addClass = async (req, res) => {
-//   const newClass = req.body;
-//   const userEmail = newClass.instructorEmail;
-//   const user = await usersCollection.findOne({ email: userEmail });
-//   if (!user) {
-//     return res.status(404).send("User not found");
-//   }
-//   if (!user.classes) {
-//     user.classes = [];
-//   }
-//   user.classes.push(newClass);
-//   const updatedUser = await usersCollection.findOneAndUpdate(
-//     { email: userEmail },
-//     { $set: user },
-//     { returnOriginal: false }
-//   );
-//   const insertedClass = await classesCollection.insertOne(newClass);
-//   res.send({
-//     user: updatedUser.value,
-//     class: insertedClass,
-//   });
-// };
+//add class
+
 module.exports.addClass = async (req, res) => {
   const newClass = req.body;
   const userEmail = newClass.instructorEmail;
@@ -41,7 +23,6 @@ module.exports.addClass = async (req, res) => {
     user.classes = [];
   }
   user.classes.push(classId);
-
   const updatedUser = await usersCollection.findOneAndUpdate(
     { email: userEmail },
     { $set: user },
@@ -52,4 +33,45 @@ module.exports.addClass = async (req, res) => {
     user: updatedUser.value,
     class: insertedClass,
   });
+};
+//update class status to approved
+
+module.exports.aprrovedClass = async (req, res) => {
+  const id = req.params.classId;
+  console.log(id);
+  const filter = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $set: {
+      status: "approved",
+    },
+  };
+  const result = await classesCollection.updateOne(filter, updateDoc);
+  res.send(result);
+};
+
+//update class status to deny
+
+module.exports.deniedClass = async (req, res) => {
+  const id = req.params.classId;
+  const filter = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $set: {
+      status: "denied",
+    },
+  };
+  const result = await classesCollection.updateOne(filter, updateDoc);
+  res.send(result);
+};
+
+//update class feedback
+module.exports.feedbackClass = async (req, res) => {
+  const id = req.params.classId;
+  const filter = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $set: {
+      feedback: req.body.feedback,
+    },
+  };
+  const result = await classesCollection.updateOne(filter, updateDoc);
+  res.send(result);
 };
--- routes/classes.js
+++ routes/classes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const classesController = require("../controllers/classes.controller");
-const { varifyJWT } = require("../middlewares/middlewares");
+const { varifyJWT, verifyAdmin } = require("../middlewares/middlewares");
 
-router.get("/", classesController.findAll);
+router.get("/", varifyJWT, verifyAdmin, classesController.findAll);
 router.post("/", classesController.addClass);
+router.patch(
+  "/approved-class/:classId",
+  varifyJWT,
+  classesController.aprrovedClass
+);
+router.patch("/denied-class/:classId", classesController.deniedClass);
+router.patch("/feedback-class/:classId", classesController.feedbackClass);
 
 module.exports = router;
