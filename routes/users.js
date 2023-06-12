c696513a222335c06e106e6c3ae82af4ebbfb1b0
shahjalal-bu <mdshahjalal.bu@gmail.com>
2023-06-12 21:59:12 +0600

    token update

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/users.controller");
 const { varifyJWT, verifyAdmin } = require("../middlewares/middlewares");
-router.get("/", varifyJWT, verifyAdmin, usersController.findAll);
+router.get("/", usersController.findAll);
 router.get("/:email", usersController.findOne);
 router.post("/", usersController.addUser);
 router.get("/check-admin/:email", varifyJWT, usersController.checkAdmin);
